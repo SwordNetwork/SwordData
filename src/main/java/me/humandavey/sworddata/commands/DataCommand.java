9e518b53e60f6e5a410a932ff1e7098f468ebf05
humandavey <davey.adams.three@gmail.com>
2024-08-14 20:57:01 -0500

    change: add data command

--- src/main/java/me/humandavey/sworddata/commands/DataCommand.java
+++ src/main/java/me/humandavey/sworddata/commands/DataCommand.java
@@ -1,2 +1,16 @@
-package me.humandavey.sworddata.commands;public class DataCommand {
+package me.humandavey.sworddata.commands;
+
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.plugin.Command;
+
+public class DataCommand extends Command {
+
+    public DataCommand() {
+        super("data");
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+
+    }
 }
